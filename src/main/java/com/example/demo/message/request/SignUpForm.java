5efb655c9df7ad5edf3325768afede3afcef2799
onssfar <60037038+onssfar@users.noreply.github.com>
2021-04-14 17:25:58 +0200

    Add files via upload

---
+++ src/main/java/com/example/demo/message/request/SignUpForm.java
@@ -0,0 +1,23 @@
+package com.example.demo.message.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+@Getter
+@Setter
+public class SignUpForm {
+
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private Set<String> role;
+
+
+
+
+}
